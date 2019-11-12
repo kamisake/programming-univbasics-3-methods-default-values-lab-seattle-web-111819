829b58dd9b4d9af87f7d3cc8a296f3ede4117cee
Charles Camisa <charles@kalekoko.com>
2019-11-12 21:36:23 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,17 +1,6 @@
-def meal_choice(veg1, veg2, protein = "meat")
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
-end
-
-meal_choice("brocolli", "macaroni")
-
-def meal_choice(veg1, veg2, protein)
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
-end
-
-meal_choice("brocolli","macaroni","tofu")
-
-def meal_choice(veg1, veg2, protein = "meat")
+def meal_choice(veg1, veg2, protein='meat')
   puts "What a nutritious meal!"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
-  return "A plate of #{protein} with #{veg1} and #{veg2}."
+  meal = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts meal
+  return meal
 end
